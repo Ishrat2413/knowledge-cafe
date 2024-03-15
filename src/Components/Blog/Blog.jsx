6dd6fedad01f0a78486dd07ebdb7c7f537ec43a9
Ishrat2413 <ishratrintu2413@gmail.com>
2024-03-15 17:02:52 +0600

    Can show the blogs with appropiate info

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-    console.log(blog)
+    const { title, cover, author, author_img, reading_time, posted_date, hashtags } = blog;
     return (
-        <div>
-
+        <div className='blog-container'>
+            <img className='w-[645px] my-5' src={cover} alt={`Cover Picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-4'>
+                    <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
+                    <div className='text-sm'>
+                        <h3 >{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-xl">{title}</h2>
+            <p className='text-sm '>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };

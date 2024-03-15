0d703b342327c72fcda7378fd52948a83f4712cf
Ishrat2413 <ishratrintu2413@gmail.com>
2024-03-15 16:44:21 +0600

    doing some internal tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <Header></Header>
-      <Blogs></Blogs>
+      <div className='w-[1170px] mx-auto mt-3'>
+        <Header></Header>
+        <div className='md:flex items-center'>
+          <Blogs></Blogs>
+          <Bookmarks></Bookmarks>
+        </div>
+      </div>
     </>
   )
 }
